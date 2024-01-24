ff85e5a765499c78fd5bb4499116c7fc30e00775
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-01-24 14:43:19 -0800

    Add files via upload

---
+++ bankProcess.m
@@ -0,0 +1,40 @@
+function [M, A, B] = bankProcess()
+
+% Preprocess the Taiwanese Credit dataset and return the centered/normalized data matrix M (with al samples)
+% and the samples of each sensitive group A and B. 
+
+addpath data/bank
+
+data = csvread('bank.csv');
+
+% Vector of sensitive attributes
+sensitive = data(:,1);
+
+% Define the sensitive attribute: 1 for aged (>= 65 years) and 0 for <65 years
+normalized = (sensitive >= 65);
+
+% Extract the sensitive attribute from the dataset
+data = data(:,2:end);
+
+[m,n] = size(data); % Number of attributes
+
+% Centering and normalizing the dataset
+data = (data - repmat(mean(data),m,1))./repmat(std(data),m,1);
+
+% Ddata for sensitive group
+data_sensitive = data(find(normalized),:);
+
+% Data for non-sensitive group
+data_nonsensitive = data(find(~normalized),:);
+
+% Centering and normalizing the sensitive attributes
+data_nonsensitive = (data_nonsensitive - repmat(mean(data_nonsensitive),size(data_nonsensitive,1),1));
+data_sensitive = (data_sensitive - repmat(mean(data_sensitive),size(data_sensitive,1),1));
+
+M = data;
+B_orig = M(find(normalized),:);
+A_orig = M(find(~normalized),:);
+A = data_nonsensitive;
+B = data_sensitive;
+
+end
