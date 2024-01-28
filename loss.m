b7f41603b529dd25cad1cee3b50c91847887fa9c
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-01-28 12:37:21 -0800

    Update loss.m

--- loss.m
+++ loss.m
@@ -1,25 +1,10 @@
 function [loss] = loss(Y, Z, d)
-%original definition of trace
 % loss of matrix Y with respect to matrix Z
 % Y and Z are of the same size
-
-Yhat = optApprox(Y, d);
-loss =  re(Y, Z) - re (Y, Yhat);
-
-function [Mhat] = optApprox(M, d)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-coeff = pca(M, 'NumComponents', d);
+coeff = pca(Y, 'NumComponents', d);
 P = coeff * transpose(coeff);
-Mhat = M*P;
-end
-
-function [reVal] = re(Y,Z)
-% Calculate the reconstruction error of matrix Y with respect to matrix Z
-% Matrix Y and Z are of the same size
-reVal = norm(Y-Z, 'fro')^2;
-end
-
+Yhat = Y * P;
+loss = (norm(Y - Z,"fro")^2 - norm(Y-Yhat,"fro")^2)/size(Y,1);
 end
 
 
