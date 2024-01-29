6870462415674902d90c49711261398756130d76
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-01-29 13:44:00 -0800

    Update main.m

--- main.m
+++ main.m
@@ -16,7 +16,7 @@ loss_XA = zeros(r_total,1);
 loss_XB = zeros(r_total,1);
 loss_XAoverXBpca = zeros(r_total,1);
 
-% Fair loss using trace definition
+% Fair loss of Fair PCA via convex optimization
 lossFair_XA = zeros(r_total,1);
 lossFair_XB = zeros(r_total,1);
 lossFair_XAoverXB = zeros(r_total,1);
@@ -24,18 +24,18 @@ lossFair_XAoverXB = zeros(r_total,1);
 % parameters of the mw algorithm
 eta = 1;
 T = 20; 
-
-% Fair loss of Samadi's algorithm
 z_last = zeros(r_total, 1);
 z = zeros(r_total, 1);
 
-loss_XAsamadi = zeros(r_total,1);
-loss_XBsamadi = zeros(r_total,1);
-loss_XAoverXBsamadi = zeros(r_total,1);
+% Fair loss of Fair PCA via convex optimization
+loss_XA_LP = zeros(r_total,1);
+loss_XB_LP = zeros(r_total,1);
+loss_XAoverXB_LP = zeros(r_total,1);
 
+% Time
 time_pca = zeros(r_total,1);
-time_fair = zeros(r_total,1);
-time_samadi = zeros(r_total,1);
+time_FairConvex = zeros(r_total,1);
+time_FairLP = zeros(r_total,1);
 
 for ell=1:r_total
 
@@ -51,78 +51,77 @@ for ell=1:r_total
     loss_XB(ell) = loss(XB,approx_XBpca,ell);
     loss_XAoverXBpca(ell) = loss_XA(ell)/loss_XB(ell);
 
-    % Fair PCA
+    % Fair PCA via convex optimization
     tic
-    [V,Ha,Hb] = fpca(XA, XB, ell, 10^(-8));
-    time_fair(ell) = toc;
+    U = fpca(XA, XB, ell, 10^(-8));
+    time_FairConvex(ell) = toc;
 
-    % Fair PCA's average loss on A and B using Samadi's definition
-    approx_XA = XA * (V * V');
-    approx_XB = XB * (V * V');
+    % the average loss on A and B of Fair PCA via convex optimization
+    approx_XA = XA * (U * U');
+    approx_XB = XB * (U * U');
     lossFair_XA(ell) = loss(XA,approx_XA,ell);
     lossFair_XB(ell) = loss(XB,approx_XB,ell);
     lossFair_XAoverXB(ell) = lossFair_XA(ell)/lossFair_XB(ell);
 
-    % Fair PCA: Samadi
+    % Fair PCA via LP
     tic
-    Psamadi = fpca_samadi(XA,XB,ell,eta,T);
-    time_samadi(ell) = toc;
-    
-    approxFair_XAsamadi = XA * Psamadi;
-    approxFair_XBsamadi = XB * Psamadi;
+    P_LP = fpca_LP(XA,XB,ell,eta,T);
+    time_FairLP(ell) = toc;
 
-    % Samadi's Fair PCA's average loss on A and B
-    loss_XAsamadi(ell) = loss(XA,approxFair_XAsamadi,ell);
-    loss_XBsamadi(ell) = loss(XB,approxFair_XBsamadi,ell);
-    loss_XAoverXBsamadi(ell) = loss_XAsamadi(ell)/loss_XBsamadi(ell); 
+    approxFair_XA_LP = XA * P_LP;
+    approxFair_XB_LP = XB * P_LP;
+
+    % the average loss on A and B of Fair PCA via LP
+    loss_XA_LP(ell) = loss(XA,approxFair_XA_LP,ell);
+    loss_XB_LP(ell) = loss(XB,approxFair_XB_LP,ell);
+    loss_XAoverXB_LP(ell) = loss_XA_LP(ell)/loss_XB_LP(ell); 
 end
 
 r_count = 1:r_total;
 r_count = r_count';
 
 T = table(r_count,lossFair_XA,lossFair_XB,lossFair_XAoverXB);
-T = table(r_count,loss_XAsamadi,loss_XBsamadi,loss_XAoverXBsamadi);
-T = table(r_count,loss_XAoverXBpca,lossFair_XAoverXB,loss_XAoverXBsamadi)
-T = table(r_count,time_pca,time_fair,time_samadi)
+T = table(r_count,loss_XA_LP,loss_XB_LP,loss_XAoverXB_LP);
+T = table(r_count,loss_XAoverXBpca,lossFair_XAoverXB,loss_XAoverXB_LP);
+T = table(r_count,time_pca,time_FairConvex,time_FairLP);
 
 % plot the fairness figure
 figure
 x = r_count;
-y1 = loss_XAoverXBpca;
-y2 = lossFair_XAoverXB;
-y3 = loss_XAoverXBsamadi;
-% plot(x,y1,'--gs',...
-%     'LineWidth',2,...
-%     'MarkerSize',10,...
-%     'MarkerEdgeColor','b',...
-%     'MarkerFaceColor',[0.5,0.5,0.5])
-% hold on
-plot(x,y2,'-rs',...
+y1 = lossFair_XAoverXB;
+y2 = loss_XAoverXB_LP;
+plot(x,y1,'-rs',...
     'LineWidth',2,...
     'MarkerSize',10,...
     'MarkerEdgeColor','k',...
     'MarkerFaceColor',[0.5,0.5,0.5])
 hold on
-plot(x,y3,'-go',...
+plot(x,y2,'-go',...
     'LineWidth',2,...
     'MarkerSize',10,...
     'MarkerEdgeColor','c',...
     'MarkerFaceColor',[0.5,0.5,0.5])
 hold off
-legend("loss ratio of our FairPCA","loss ratio of Samadi's FairPCA")
+legend("FairPCA via convex optimization","FairPCA via LP")
 xlabel("Number of dimensions")
 ylabel("Loss")
 title("Loss of algorithms")
+%print -depsc newfigure1
 
 % plot the time figure
 figure
 x = r_count; 
 y1 = time_pca; plot(x,y1,"b--|",'LineWidth',3);
 hold on
-y2 = time_fair; plot(x,y2,"r-s",'LineWidth',3);
+y2 = time_FairConvex; plot(x,y2,"r-s",'LineWidth',3);
 hold on
-y3 = time_samadi; plot(x,y3,"g-o",'LineWidth',3);
+y3 = time_FairLP; plot(x,y3,"g-o",'LineWidth',3);
 hold off
+legend("Vanilla PCA", "FairPCA via convex optimization","FairPCA via LP")
+xlabel("Number of dimensions")
+ylabel("Running time")
+title("Running time of algorithms")
+%print -depsc newfigure2
 legend("time PCA", "time Fair","time Samadi")
 xlabel("Number of dimensions")
 ylabel("Running time")
