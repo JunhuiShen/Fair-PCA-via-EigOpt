af522181b34666c2e4013cbc335d3f46c3a222ac
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-01-30 13:05:16 -0800

    Update loss.m

--- loss.m
+++ loss.m
@@ -2,8 +2,7 @@ function [loss] = loss(Y, Z, r)
 % loss of matrix Y with respect to matrix Z
 % Y and Z are of the same size
 coeff = pca(Y, 'NumComponents', r);
-P = coeff * transpose(coeff);
-Yhat = Y * P;
+Yhat = Y * (coeff * transpose(coeff));
 loss = (norm(Y - Z,"fro")^2 - norm(Y-Yhat,"fro")^2)/size(Y,1);
 end
 
