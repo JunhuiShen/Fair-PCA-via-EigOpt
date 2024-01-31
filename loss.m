20e02dd0f71d339f6d5665064f9f192f72fc3d52
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-01-31 08:30:41 -0800

    Update loss.m

--- loss.m
+++ loss.m
@@ -1,6 +1,6 @@
 function [loss] = loss(Y, Z, r)
-% loss of matrix Y with respect to matrix Z
-% Y and Z are of the same size
+% Compute ||Y - Z||_{F}^{2} - ||Y - Yhat||_{F}^{2}
+% where Yhat is the best rank-r approximation of Y
 coeff = pca(Y, 'NumComponents', r);
 Yhat = Y * (coeff * transpose(coeff));
 loss = (norm(Y - Z,"fro")^2 - norm(Y-Yhat,"fro")^2)/size(Y,1);
