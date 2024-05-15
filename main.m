9c9b2c923dbe593689aa0c9b30fd1617f0d2a631
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-05-14 20:55:28 -0700

    Update main.m

--- main.m
+++ main.m
@@ -1,38 +1,38 @@
-clc; close;clear; rng("default"); warning('off','all');warning;
+clc; close;clear; rng("default"); warning('off','all');warning;format short
 
 % Data set
-[X, XA, XB] = bankProcess();
-% [X, XA, XB] = creditProcess();
-% [X, XA, XB] = LFWProcess();
-% [X,XA,XB] = cropProcess();
+[M, A, B] = bankProcess();
+% [M, A, B] = creditProcess();
+% [M,A,B] = cropProcess();
+% [M, A, B] = LFWProcess();
 
 % Total reduced dimension
 r_total = 10; 
 
 % Extract the dimension
-d = size(XA,2);
-na = size(XA,1);
-nb = size(XB,1);
+d = size(A,2);
+na = size(A,1);
+nb = size(B,1);
 
-% Vanilla loss
-loss_XA = zeros(r_total,1);
-loss_XB = zeros(r_total,1);
-loss_XAoverXBpca = zeros(r_total,1);
+% Vanilla PCA loss
+loss_A = zeros(r_total,1);
+loss_B = zeros(r_total,1);
+loss_AoverBpca = zeros(r_total,1);
 
 % Loss of Fair PCA via eigenvalue Optimization
-lossFair_XA = zeros(r_total,1);
-lossFair_XB = zeros(r_total,1);
+lossFair_A = zeros(r_total,1);
+lossFair_B = zeros(r_total,1);
 lossFair_max = zeros(r_total,1);
-lossFair_XAoverXB = zeros(r_total,1);
+lossFair_AoverB = zeros(r_total,1);
 
 % Parameter of Fair PCA via eigenvalue Optimization
 tol = 10^(-8);
 
 % Loss of Fair PCA via LP
-loss_XA_LP = zeros(r_total,1);
-loss_XB_LP = zeros(r_total,1);
+loss_A_LP = zeros(r_total,1);
+loss_B_LP = zeros(r_total,1);
 loss_LP_max = zeros(r_total,1);
-loss_XAoverXB_LP = zeros(r_total,1);
+loss_AoverB_LP = zeros(r_total,1);
 
 % Parameters of Fair PCA via LP
 eta = 1;
@@ -40,56 +40,60 @@ T = 20;
 z_last = zeros(r_total, 1);
 z = zeros(r_total, 1);
 
-% Efficiency
+% Runtime
 time_pca = zeros(r_total,1);
-time_FairConvex = zeros(r_total,1);
+time_Fair = zeros(r_total,1);
 time_FairLP = zeros(r_total,1);
+time_ratio = zeros(r_total,1);
 
 for r=1:r_total
 
     % Vanilla PCA 
     tic
-    coeff = pca(X,"NumComponents",r);
+    coeff = pca(M,"NumComponents",r);
     time_pca(r) = toc;
     
     % Projection of Vanilla PCA
-    approx_XApca = XA * (coeff * coeff');
-    approx_XBpca = XB * (coeff * coeff');
+    approx_Apca = A * (coeff * coeff');
+    approx_Bpca = B * (coeff * coeff');
 
     % The average loss on A and B of Vanilla PCA
-    loss_XA(r) = loss(XA,approx_XApca,r);
-    loss_XB(r) = loss(XB,approx_XBpca,r);
-    loss_XAoverXBpca(r) = loss_XA(r)/loss_XB(r);
+    loss_A(r) = loss(A,approx_Apca,r);
+    loss_B(r) = loss(B,approx_Bpca,r);
+    losspca_max(r) = max(loss_A(r),loss_B(r));
+    loss_AoverBpca(r) = loss_A(r)/loss_B(r);
 
     % Fair PCA via eigenvalue Optimization
     tic
-    U = fpca_Eigenvalue_Optimization(XA, XB, r,tol);
-    time_FairConvex(r) = toc;
+    U = fpca_Eigenvalue_Optimization(A, B, r,tol);
+    time_Fair(r) = toc;
 
     % Projection of Fair PCA via eigenvalue Optimization
-    approx_XA = XA * (U * U');
-    approx_XB = XB * (U * U');
+    approx_A = A * (U * U');
+    approx_B = B * (U * U');
 
     % The average loss on A and B of Fair PCA via eigenvalue Optimization
-    lossFair_XA(r) = loss(XA,approx_XA,r);
-    lossFair_XB(r) = loss(XB,approx_XB,r);
-    lossFair_max(r) = max(lossFair_XA(r),lossFair_XB(r));
-    lossFair_XAoverXB(r) = lossFair_XA(r)/lossFair_XB(r);
+    lossFair_A(r) = loss(A,approx_A,r);
+    lossFair_B(r) = loss(B,approx_B,r);
+    lossFair_max(r) = max(lossFair_A(r),lossFair_B(r));
+    lossFair_AoverB(r) = lossFair_A(r)/lossFair_B(r);
 
     % Fair PCA via LP
     tic
-    P_LP = fpca_LP(XA,XB,r,eta,T);
+    P_LP = fpca_LP(A,B,r,eta,T);
     time_FairLP(r) = toc;
     
     % Projection of Fair PCA via LP
-    approxFair_XA_LP = XA * P_LP;
-    approxFair_XB_LP = XB * P_LP;
+    approxFair_A_LP = A * P_LP;
+    approxFair_B_LP = B * P_LP;
 
     % The average loss on A and B of Fair PCA via LP
-    loss_XA_LP(r) = loss(XA,approxFair_XA_LP,r);
-    loss_XB_LP(r) = loss(XB,approxFair_XB_LP,r);
-    loss_LP_max(r) = max(loss_XA_LP(r),loss_XB_LP(r));
-    loss_XAoverXB_LP(r) = loss_XA_LP(r)/loss_XB_LP(r); 
+    loss_A_LP(r) = loss(A,approxFair_A_LP,r);
+    loss_B_LP(r) = loss(B,approxFair_B_LP,r);
+    loss_LP_max(r) = max(loss_A_LP(r),loss_B_LP(r));
+    loss_AoverB_LP(r) = loss_A_LP(r)/loss_B_LP(r); 
+
+    time_ratio(r) = time_FairLP(r)/time_Fair(r);
 end
 
 % Make a list for each reduced dimension
@@ -97,17 +101,16 @@ r_count = 1:r_total;
 r_count = r_count';
 
 % Table of different comparison result
-T = table(r_count,lossFair_XA,lossFair_XB,lossFair_XAoverXB);
-T = table(r_count,loss_XA_LP,loss_XB_LP,loss_XAoverXB_LP);
-T = table(r_count,lossFair_max,loss_LP_max);
-T = table(r_count,loss_XAoverXBpca,lossFair_XAoverXB,loss_XAoverXB_LP);
-T = table(r_count,time_pca,time_FairConvex,time_FairLP);
+% T = table(r_count,lossFair_A,lossFair_B,lossFair_AoverB);
+% T = table(r_count,loss_A_LP,loss_B_LP,loss_AoverB_LP);
+% T = table(r_count,lossFair_max,loss_LP_max);
+% T = table(r_count,loss_AoverBpca,lossFair_AoverB,loss_AoverB_LP);
 
-% Plot the loss ratio figure
+% Plot the reconstruction loss ratio figure
 figure
 x = r_count;
-y1 = lossFair_XAoverXB;
-y2 = loss_XAoverXB_LP;
+y1 = lossFair_AoverB;
+y2 = loss_AoverB_LP;
 plot(x,y1,'-rs',...
     'LineWidth',2,...
     'MarkerSize',10,...
@@ -120,17 +123,17 @@ plot(x,y2,'-go',...
     'MarkerEdgeColor','c',...
     'MarkerFaceColor',[0.5,0.5,0.5])
 hold off
-legend("FairPCA via eigenvalue optimization","FairPCA via LP")
+legend("FPCA via eigenvalue optimization","FPCA via LP")
 xlabel("Number of reduced dimensions")
-ylabel("Loss ratio")
-title("Loss Ratio")
+ylabel("Reconstruction loss ratio")
+title("Reconstruction loss Ratio")
 % print -depsc newfigure1
 
-% Plot the fairness measure figure
+% Plot the objective value figure
 figure
 x = r_count;
 y1 = lossFair_max;
-y2 = loss_LP_max;
+y2 = losspca_max;
 plot(x,y1,'-rs',...
     'LineWidth',2,...
     'MarkerSize',10,...
@@ -143,23 +146,26 @@ plot(x,y2,'-go',...
     'MarkerEdgeColor','c',...
     'MarkerFaceColor',[0.5,0.5,0.5])
 hold off
-legend("FairPCA via eigenvalue optimization","FairPCA via LP")
+legend("FPCA via eigenvalue optimization","Vanilla PCA")
 xlabel("Number of reduced dimensions")
-ylabel("Fairness measure")
-title("Fairness measure")
+ylabel("Objective value")
+title("Objective value")
 % print -depsc newfigure2
 
-% Plot the efficiency figure
-figure
-x = r_count; 
-y1 = time_pca; plot(x,y1,"b--|",'LineWidth',3);
-hold on
-y2 = time_FairConvex; plot(x,y2,"r-s",'LineWidth',3);
-hold on
-y3 = time_FairLP; plot(x,y3,"g-o",'LineWidth',3);
-hold off
-legend("Vanilla PCA", "FPCA via eigenvalue optimization","FPCA via LP")
-xlabel("Number of reduced dimensions")
-ylabel("Running time")
-title("Running time")
-% print -depsc newfigure3
+
+T = table(r_count,time_pca,time_Fair,time_FairLP,time_ratio)
+
+% % Plot the efficiency figure
+% figure
+% x = r_count; 
+% y1 = time_pca; plot(x,y1,"b--|",'LineWidth',3);
+% hold on
+% y2 = time_Fair; plot(x,y2,"r-s",'LineWidth',3);
+% hold on
+% y3 = time_FairLP; plot(x,y3,"g-o",'LineWidth',3);
+% hold off
+% legend("Vanilla PCA", "FPCA via eigenvalue optimization","FPCA via LP")
+% xlabel("Number of reduced dimensions")
+% ylabel("Runtime")
+% title("Runtime")
+% % print -depsc newfigure3
