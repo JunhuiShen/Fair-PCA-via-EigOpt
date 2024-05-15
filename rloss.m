eb7d3ea4202b96bd1372260d94d0d4806799b57c
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-05-14 21:13:21 -0700

    Update and rename loss.m to rloss.m

--- rloss.m
+++ rloss.m
@@ -1,10 +1,11 @@
-function [loss] = loss(Y, Z, r)
+function [rloss] = rloss(Y, Z, r)
 % Compute ||Y - Z||_{F}^{2} - ||Y - Yhat||_{F}^{2}
 % where Yhat is the best rank-r approximation of Y
 coeff = pca(Y, 'NumComponents', r);
 Yhat = Y * (coeff * transpose(coeff));
-loss = (norm(Y - Z,"fro")^2 - norm(Y-Yhat,"fro")^2)/size(Y,1);
+rloss = (norm(Y - Z,"fro")^2 - norm(Y-Yhat,"fro")^2)/size(Y,1);
 end
+
 
 
 
