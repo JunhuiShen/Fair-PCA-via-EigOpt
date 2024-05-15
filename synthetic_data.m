01b28a65a548065fb850435ac00ff34a28a4840e
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-05-14 20:57:06 -0700

    Update synthetic_data.m

--- synthetic_data.m
+++ synthetic_data.m
@@ -1,57 +1,58 @@
 clc; close; clear;  rng("default");  warning('off','all');warning;
 
 % parameter
-N = 35; 
-XA_mean = [0, 0, 0]; 
-XB_mean = [0, 0, 0]; 
+NA = 35; 
+NB = 35;
+A_mean = [0, 0, 0]; 
+MB_mean = [0, 0, 0]; 
 cov_matrix1 = [1, -0.8, 0.5; -0.8, 1, -0.5; 0.5, -0.5, 1]; 
-cov_matrix2 = [1, 0.8, -0.2; 0.8, 1, 0.3; -0.2, 0.3, 1]; 
+cov_matrix2 = 0.5 * [1, 0.8, -0.2; 0.8, 1, 0.3; -0.2, 0.3, 1]; 
 r = 1; tol = 10^(-8);
 
 % Generate synthetic data
-XA = mvnrnd(XA_mean, cov_matrix1, N);
-XB = mvnrnd(XB_mean, cov_matrix2, N);
-X = [XA;XB];
+A = mvnrnd(A_mean, cov_matrix1, NA);
+B = mvnrnd(MB_mean, cov_matrix2, NB);
+M = [A;B];
 
 % Vanilla PCA
-coeff = pca(X,"NumComponents",r);
-approx_XApca = XA * (coeff * coeff');
-approx_XBpca = XB * (coeff * coeff');
-approx_Xpca = X * (coeff * coeff');
+coeff = pca(M,"NumComponents",r);
+approx_Apca = A * (coeff * coeff');
+approx_MBpca = B * (coeff * coeff');
+approx_Mpca = M * (coeff * coeff');
 
 % Fair PCA
-U = fpca_Eigenvalue_Optimization(XA, XB, r,tol);
-approx_XA = XA *(U * U');
-approx_XB = XB *(U * U');
-approx_X = X *(U * U');
+U = fpca_Eigenvalue_Optimization(A, B, r,tol);
+approx_A = A *(U * U');
+approx_B = B *(U * U');
+approx_M = M *(U * U');
 
-approx_Xpca1 = approx_Xpca(:,1);
-approx_Xpca2 = approx_Xpca(:,2);
-[leftmost_approx_Xpca,idx1] = min(approx_Xpca1);
-[rightmost_approx_Xpca,idx2] = max(approx_Xpca1);
-xline1 = [leftmost_approx_Xpca,rightmost_approx_Xpca];
-yline1 = [approx_Xpca2(idx1),approx_Xpca2(idx2)];
+approx_Mpca1 = approx_Mpca(:,1);
+approx_Mpca2 = approx_Mpca(:,2);
+[leftmost_approx_Mpca,idx1] = min(approx_Mpca1);
+[rightmost_approx_Mpca,idx2] = max(approx_Mpca1);
+xline1 = [leftmost_approx_Mpca,rightmost_approx_Mpca];
+yline1 = [approx_Mpca2(idx1),approx_Mpca2(idx2)];
 
-approx_X1 = approx_X(:,1);
-approx_X2 = approx_X(:,2);
-[leftmost_approx_X,idx1] = min(approx_X1);
-[rightmost_approx_X,idx2] = max(approx_X1);
-xline2 = [leftmost_approx_X,rightmost_approx_X];
-yline2 = [approx_X2(idx1),approx_X2(idx2)];
+approx_M1 = approx_M(:,1);
+approx_M2 = approx_M(:,2);
+[leftmost_approx_M,idx1] = min(approx_M1);
+[rightmost_approx_M,idx2] = max(approx_M1);
+xline2 = [leftmost_approx_M,rightmost_approx_M];
+yline2 = [approx_M2(idx1),approx_M2(idx2)];
 
 figure
-scatter3(XA(:,1), XA(:,2), XA(:,3), 100, "g", "o", "filled");
+scatter3(A(:,1), A(:,2), A(:,3), 100, "g", "o", "filled");
 hold on 
-scatter3(XB(:,1), XB(:,2), XB(:,3), 100, "c", "s", "filled");
+scatter3(B(:,1), B(:,2), B(:,3), 100, "c", "s", "filled");
 hold on
-scatter3(approx_Xpca(:,1), approx_Xpca(:,2), approx_Xpca(:,3), 100, [0.4940 0.1840 0.5560], "*");
-% plot3(approx_Xpca(:,1), approx_Xpca(:,2), approx_Xpca(:,3), "LineWidth", 2)
+% scatter3(approx_Mpca(:,1), approx_Mpca(:,2), approx_Mpca(:,3), 100, [0.4940 0.1840 0.5560], "*");
+plot3(approx_Mpca(:,1), approx_Mpca(:,2), approx_Mpca(:,3), "LineWidth", 2)
 hold on
-scatter3(approx_X(:,1), approx_X(:,2), approx_X(:,3), 100, "r", "o");
-% plot3(approx_X(:,1), approx_X(:,2), approx_X(:,3), "LineWidth", 2)
+% scatter3(approx_M(:,1), approx_M(:,2), approx_M(:,3), 100, "r", "o");
+plot3(approx_M(:,1), approx_M(:,2), approx_M(:,3), "LineWidth", 2)
 hold off
 xlabel("x")
 ylabel("y")
 zlabel("z")
-legend("X_A","X_B","Vanilla PCA","Fair PCA")
+legend("A","B","Vanilla PCA","Fair PCA")
 title("Comparison between Vanilla PCA and Fair PCA")
