6e0f483932f7657d631757600a9299c32952b0d0
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-06-04 11:06:27 -0700

    Add files via upload

--- rloss.m
+++ rloss.m
@@ -1,9 +1,9 @@
-function [rloss] = rloss(Y, Z, r)
-% Compute ||Y - Z||_{F}^{2} - ||Y - Yhat||_{F}^{2}
-% where Yhat is the best rank-r approximation of Y
-coeff = pca(Y, 'NumComponents', r);
-Yhat = Y * (coeff * transpose(coeff));
-rloss = (norm(Y - Z,"fro")^2 - norm(Y-Yhat,"fro")^2)/size(Y,1);
+function [rloss] = rloss(M, Mapprox, r)
+% Compute the average reconstruction loss
+% where Mbestapprox is the best rank-r approximation of M
+U = pca(M, 'NumComponents', r);
+Mbestapprox = M * (U * U');
+rloss = (norm(M - Mapprox,"fro")^2 - norm(M-Mbestapprox,"fro")^2)/size(M,1);
 end
 
 
