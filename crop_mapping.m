c1293ea10d1d47a8a520a198a6aa0bcb6d702f19
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-06-04 11:04:54 -0700

    Add files via upload

--- crop_mapping.m
+++ crop_mapping.m
@@ -1,5 +1,7 @@
 function [M, A, B] = crop_mapping()
+% Load and preprocess the Crop Mapping dataset
 
+% Load the dataset from the CSV file
 data = readtable('crop.csv');
 data = data{:,:};
 data = sortrows(data);
@@ -7,28 +9,31 @@ data = sortrows(data);
 % Vector of sensitive attributes
 sensitive = data(:,1);
 
-% Define the sensitive attribute: Corns
+% Define the sensitive attribute: Corns (1 for corn, 0 for other crops)
 normalized = (sensitive == 1);
 
 % Extract the sensitive attribute from the dataset
 data = data(:,2:end-1);
 
-[m,n] = size(data); % Number of attributes
+% Get the number of instances (m) and features (n)
+[m,n] = size(data);
 
-% Centering and normalizing the dataset
-data = (data - repmat(mean(data),m,1))./repmat(std(data),m,1);
+% Center and normalize the dataset
+data = (data - repmat(mean(data), m, 1)) ./ repmat(std(data), m, 1);
 
 % Data for corn
-data_corn = data(find(normalized),:);
+data_corn = data(find(normalized), :);
 
 % Data for other crops
-data_other_crops = data(find(~normalized),:);
+data_other_crops = data(find(~normalized), :);
 
-% Centering and normalizing the sensitive attributes
-data_corn = (data_corn - repmat(mean(data_corn),size(data_corn,1),1));
-data_other_crops = (data_other_crops - repmat(mean(data_other_crops),size(data_other_crops,1),1));
+% Center and normalize the sensitive attributes
+data_corn = (data_corn - repmat(mean(data_corn), size(data_corn, 1), 1));
+data_other_crops = (data_other_crops - repmat(mean(data_other_crops), size(data_other_crops, 1), 1));
 
+% Assign the data to output variables
 A = data_corn;
 B = data_other_crops;
-M = [A;B];
+M = [A; B];
+
 end
